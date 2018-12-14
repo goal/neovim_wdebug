2deb8f85e79b0f97127471457611d9b51580412e
王彦金 <wyj0056@175game.com>
2018-12-14 15:34:49 +0800

    Rup cmd

--- rplugin/python3/wdebug.py
+++ rplugin/python3/wdebug.py
@@ -1,20 +1,24 @@
-import neovim
+import os
+from pathlib import Path
+import time
+import pynvim as nvim
 
 
-@neovim.plugin
+@nvim.plugin
 class Work(object):
+
     def __init__(self, vim):
         self.vim = vim
         self.vim.command('nnoremap <C-x>j :Rd<cr>')
         self.vim.command('nnoremap <C-x>k :Rdv<cr>')
 
-    @neovim.command('Rdv', range='', nargs='*', sync=True)
+    @nvim.command('Rdv', range='', nargs='*', sync=True)
     def rdv_cmd(self, args, range):
         line = self.vim.current.line
         words = line.split()
         self._debug_values(words)
 
-    @neovim.command('Rd', range='', nargs='*', sync=True)
+    @nvim.command('Rd', range='', nargs='*', sync=True)
     def rd_cmd(self, args, range):
         line = self.vim.current.line
         s = line.replace("...", "").replace("*", "")
@@ -24,6 +28,21 @@ class Work(object):
             t, v = x.strip().split()
             vs.append(v)
         self._debug_values(vs)
+
+    @nvim.command('Ru', range='', nargs='*', sync=True)
+    def ru_cmd(self, args, range):
+        filename = self.vim.current.buffer.name
+        cur_dir = Path(".").absolute()
+        files = cur_dir.glob("**/online.log")
+
+        for f in files:
+            st = f.stat()
+            if now - st.st_mtime < 6:
+                self.vim.out_write("running gamed %s" % f.parent.parent)
+                break
+        else:
+            self.vim.out_write("no running gamed found!")
+            return
 
     def _debug_values(self, values):
         line = "debug_message(\"" + ", ".join(
